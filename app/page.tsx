b09c07abb489ad759e4e0daa077b7a002e05366e
Your Name <you@example.com>
2025-04-13 15:50:53 -0400

    changed 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-import Paralax from "./(routes)/panels/Paralax"
-
+import Paralax from "./(routes)/panels/Paralax";
 
 export default function Home() {
   return (
     <div className="flex flex-col h-screen justify-center  m-20">
-<div className="flex justify-center">
-  <div className=" max-w-72 min-w-72">
-
-         <h1 className="text-red-600 text-center">Note Important!</h1>
-         <div>
-             <span>Plis suscribete a mi canal de youtube para poder seguir creando paneles y ponerselos sin anuncios con links directos <p>Les agradeceria un  like & suscribirse</p></span>
-
-         </div>
-  </div>
-</div>
-        <Paralax/>
+      <div className="flex justify-center">
+        <div className=" max-w-72 min-w-72">
+          <h1 className="text-red-600 text-center">Nota Importante!</h1>
+          <div>
+            <span>
+              Plis suscribete a mi canal de youtube para poder seguir y  sin anuncios con links directos{" "}
+              <p>Les agradeceria un like & suscribirse </p>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Paralax />
+      <p className="text-center"> 
+        Creditos a <strong className="text-red-500">Opti</strong>
+      </p>
     </div>
-  )
+  );
 }
